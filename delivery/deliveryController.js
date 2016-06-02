f2194e286239beee6ef92a529e986258bd53c28d
pratik9909102208ece <pratik9909102208ece@gmail.com>
2016-06-02 17:06:20 +0530

    minor css changes

--- delivery/deliveryController.js
+++ delivery/deliveryController.js
@@ -15,11 +15,11 @@ app.controller('deliveryController', ['$scope', function ($scope) {
         $scope.costPrice = $scope.shares * $scope.costPerShare;
         $scope.sellingPrice = $scope.shares * $scope.sellPerShare;
         $scope.turnover = $scope.costPrice + $scope.sellingPrice;
-        $scope.brokerage = convertPercenttoNumber($scope.brokeragePercentage) * $scope.costPrice + convertPercenttoNumber($scope.brokeragePercentage) * $scope.sellingPrice;
-        $scope.serviceTax = convertPercenttoNumber(15) * $scope.brokerage;
+        $scope.brokerage = convertPercenttoNumber($scope.brokeragePercentage) * $scope.costPrice + convertPercenttoNumber($scope.brokeragePercentage) * $scope.sellingPrice;        
         $scope.stt = convertPercenttoNumber(0.025) * $scope.sellingPrice;
         $scope.stampDuty = convertPercenttoNumber(0.002) * $scope.turnover;
         $scope.transactionCharges = convertPercenttoNumber($scope.exchangeRate) * $scope.turnover;
+        $scope.serviceTax = convertPercenttoNumber(15) * ($scope.brokerage + $scope.transactionCharges);
         $scope.profitOrLoss = $scope.sellingPrice - ($scope.costPrice + $scope.brokerage + $scope.serviceTax + $scope.stt + $scope.stampDuty + $scope.transactionCharges);
         $scope.sebiCharges = 1.68 + ($scope.turnover - $scope.turnover);
         $scope.profit = ($scope.profitOrLoss >= 0 || isNaN($scope.profitOrLoss)) ? true : false;
--- intraday/intradayController.js
+++ intraday/intradayController.js
@@ -16,10 +16,10 @@ app.controller('intradayController', ['$scope', function ($scope) {
         $scope.sellingPrice = $scope.shares * $scope.sellPerShare;
         $scope.turnover = $scope.costPrice + $scope.sellingPrice;
         $scope.brokerage = convertPercenttoNumber($scope.brokeragePercentage) * $scope.costPrice + convertPercenttoNumber($scope.brokeragePercentage) * $scope.sellingPrice;
-        $scope.serviceTax = convertPercenttoNumber(15) * $scope.brokerage;
         $scope.stt = convertPercenttoNumber(0.025) * $scope.sellingPrice;
         $scope.stampDuty = convertPercenttoNumber(0.002) * $scope.turnover;
         $scope.transactionCharges = convertPercenttoNumber($scope.exchangeRate) * $scope.turnover;
+        $scope.serviceTax = convertPercenttoNumber(15) * ($scope.brokerage + $scope.transactionCharges);    
         $scope.profitOrLoss = $scope.sellingPrice - ($scope.costPrice + $scope.brokerage + $scope.serviceTax + $scope.stt + $scope.stampDuty + $scope.transactionCharges);
         $scope.sebiCharges = 1.68 + ($scope.turnover - $scope.turnover);
         $scope.profit = ($scope.profitOrLoss >= 0 || isNaN($scope.profitOrLoss)) ? true : false;
