0e1fd9da52b4b5748f4c3bf446e332f742e18c2f
pratik9909102208ece <pratik9909102208ece@gmail.com>
2016-06-02 13:02:14 +0530

    Restrucured project

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-var app = angular.module("tax",[]);
+var app = angular.module('tax',['ui.router']);
 
-/*app.config($routeProvider){
+app.config(function($stateProvider,$urlRouterProvider){
     
+    $urlRouterProvider.otherwise('/home');
     
+    $stateProvider
     
-}*/
+    .state('home',{
+        url: '/home',
+        templateUrl: '/intraday/intraday.html',
+        controller: 'intradayController',
+        controllerAs: 'intraCtrl'        
+    });
+    
+
+});
---
+++ intraday/intradayController.js
@@ -0,0 +1,27 @@
+app.controller('intradayController', ['$scope', function ($scope) {
+    $scope.shares;
+    $scope.costPerShare;
+    $scope.sellPerShare;
+    $scope.brokeragePercentage;
+    $scope.sebiCharges;
+    $scope.profit;
+
+
+    var convertPercenttoNumber = function (percent) {
+        return percent * 0.01;
+    };
+
+    $scope.$watchGroup(['shares', 'costPerShare', 'sellPerShare', 'brokeragePercentage', 'exchangeRate'], function () {
+        $scope.costPrice = $scope.shares * $scope.costPerShare;
+        $scope.sellingPrice = $scope.shares * $scope.sellPerShare;
+        $scope.turnover = $scope.costPrice + $scope.sellingPrice;
+        $scope.brokerage = convertPercenttoNumber($scope.brokeragePercentage) * $scope.costPrice + convertPercenttoNumber($scope.brokeragePercentage) * $scope.sellingPrice;
+        $scope.serviceTax = convertPercenttoNumber(15) * $scope.brokerage;
+        $scope.stt = convertPercenttoNumber(0.025) * $scope.sellingPrice;
+        $scope.stampDuty = convertPercenttoNumber(0.002) * $scope.turnover;
+        $scope.transactionCharges = convertPercenttoNumber($scope.exchangeRate) * $scope.turnover;
+        $scope.profitOrLoss = $scope.sellingPrice - ($scope.costPrice + $scope.brokerage + $scope.serviceTax + $scope.stt + $scope.stampDuty + $scope.transactionCharges);
+        $scope.sebiCharges = 1.68 + ($scope.turnover - $scope.turnover);
+        $scope.profit = ($scope.profitOrLoss >= 0 || isNaN($scope.profitOrLoss)) ? true : false;
+    });
+}]);
