b986243b1e0da82ef17bf22165c64a8183aed4a8
pratik9909102208ece <pratik9909102208ece@gmail.com>
2016-06-03 12:49:14 +0530

    removed redundant code

--- app.js
+++ app.js
@@ -2,21 +2,12 @@ var app = angular.module('tax',['ui.router']);
 
 app.config(function($stateProvider,$urlRouterProvider){
     
-    $urlRouterProvider.otherwise('/intra');
+    $urlRouterProvider.otherwise('/home');
     
-    $stateProvider
-    
-    .state('intra',{
+    $stateProvider.state('home',{
         url: '/intra',
         templateUrl: '/intraday/intraday.html',
         controller: 'intradayController'        
-    })
-    
-    .state('delivery',{
-        url: '/delivery',
-        templateUrl: '/delivery/delivery.html',
-        controller: 'deliveryController'        
     });
-    
 
 });
--- intraday/intradayController.js
+++ intraday/intradayController.js
@@ -1,27 +1,47 @@
-app.controller('intradayController', ['$scope', function ($scope) {
+app.controller('intradayController', ['$scope', function ($scope) {    
     $scope.shares;
     $scope.costPerShare;
     $scope.sellPerShare;
     $scope.brokeragePercentage;
     $scope.sebiCharges;
-    $scope.profit=true;
+    $scope.profit= true;
+    $scope.intra= true;
+    $scope.sttPercent = 0.025;
+    var costPrice;
+    var sellingPrice;
 
 
     var convertPercenttoNumber = function (percent) {
         return percent * 0.01;
     };
+    
+    $scope.setIntraday = function(){
+        $scope.intra= true;
+        $scope.sttPercent = 0.025;
+    }
+    
+    $scope.setDelivery= function(){
+        $scope.intra= false;
+        $scope.sttPercent = 0.1;
+    }
 
-    $scope.$watchGroup(['shares', 'costPerShare', 'sellPerShare', 'brokeragePercentage', 'exchangeRate'], function () {
-        $scope.costPrice = $scope.shares * $scope.costPerShare;
-        $scope.sellingPrice = $scope.shares * $scope.sellPerShare;
-        $scope.turnover = $scope.costPrice + $scope.sellingPrice;
-        $scope.brokerage = convertPercenttoNumber($scope.brokeragePercentage) * $scope.costPrice + convertPercenttoNumber($scope.brokeragePercentage) * $scope.sellingPrice;
-        $scope.stt = convertPercenttoNumber(0.025) * $scope.sellingPrice;
+    $scope.$watchGroup(['shares', 'costPerShare', 'sellPerShare', 'brokeragePercentage', 'exchangeRate','intra'], function () {
+        costPrice = $scope.shares * $scope.costPerShare;
+        sellingPrice = $scope.shares * $scope.sellPerShare;
+        $scope.turnover = costPrice + sellingPrice;
+        $scope.brokerage = convertPercenttoNumber($scope.brokeragePercentage) * costPrice + convertPercenttoNumber($scope.brokeragePercentage) * sellingPrice;
+        if ($scope.intra){
+            $scope.stt = convertPercenttoNumber($scope.sttPercent) * sellingPrice;    
+        }        
+        else {
+            $scope.stt = convertPercenttoNumber($scope.sttPercent) * $scope.turnover;    
+        }                        
         $scope.stampDuty = convertPercenttoNumber(0.002) * $scope.turnover;
         $scope.transactionCharges = convertPercenttoNumber($scope.exchangeRate) * $scope.turnover;
         $scope.serviceTax = convertPercenttoNumber(15) * ($scope.brokerage + $scope.transactionCharges);    
-        $scope.profitOrLoss = $scope.sellingPrice - ($scope.costPrice + $scope.brokerage + $scope.serviceTax + $scope.stt + $scope.stampDuty + $scope.transactionCharges);
+        $scope.profitOrLoss = sellingPrice - (costPrice + $scope.brokerage + $scope.serviceTax + $scope.stt + $scope.stampDuty + $scope.transactionCharges);
         $scope.sebiCharges = 1.68 + ($scope.turnover - $scope.turnover);
         $scope.profit = ($scope.profitOrLoss >= 0 || isNaN($scope.profitOrLoss)) ? true : false;
-    });
+    });    
+    
 }]);
